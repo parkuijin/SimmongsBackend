97c366089a39bc96eac18c76b9b6532eba3eb04b
uijinpark <uj.park@icloud.com>
2023-09-27 14:34:05 +0900

    Create Project

---
+++ src/main/java/com/simmongs/SimmongsApplication.java
@@ -0,0 +1,13 @@
+package com.simmongs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimmongsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimmongsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simmongs/SimmongsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simmongs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimmongsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
