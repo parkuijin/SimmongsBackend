7d1d316efe3a275fc3d46d7e5a07242aaa1483ab
uijinpark <uj.park@icloud.com>
2023-10-29 12:20:50 +0900

    feat : WorkPerformance 재고처리 로직 추기, 오류처리 추가

--- src/main/java/com/simmongs/workperformance/WorkPerformanceController.java
+++ src/main/java/com/simmongs/workperformance/WorkPerformanceController.java
@@ -1,12 +1,16 @@
 package com.simmongs.workperformance;
 
+import com.simmongs.bom.BOMRepository;
+import com.simmongs.bom.BOMs;
 import com.simmongs.workorder.WorkOrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,6 +23,28 @@ public class WorkPerformanceController {
 
     private final WorkPerformanceRepository workPerformanceRepository;
     private final WorkPerformanceService workPerformanceService;
+    private final WorkOrderRepository workOrderRepository;
+    private final BOMRepository bomRepository;
+
+    @GetMapping("mrpCalculation") // 제품 완성 개수 입력하면 소모된 부품 개수 계산
+    public List<HashMap<String, Object>> MRPCalculation(@RequestBody String json) throws JSONException {
+
+        JSONObject obj = new JSONObject(json);
+        Long workOrderId = Long.parseLong(obj.getString("workOrderId"));
+        Integer currentWorkload = Integer.parseInt(obj.getString("currentWorkload"));
+
+        List<HashMap<String, Object>> response = new ArrayList<HashMap<String, Object>>();
+
+        List<BOMs> boMsList = bomRepository.findByProductCode(workOrderRepository.getByWorkOrderId(workOrderId).getProductCode());
+        for (BOMs boms : boMsList) {
+            HashMap<String, Object> hashMap = new HashMap<>();
+            hashMap.put("usedProductCode", boms.getChildProductCode());
+            hashMap.put("usedProductAmount", boms.getBomAmount()*currentWorkload);
+            response.add(hashMap);
+        }
+
+        return response;
+    }
 
     @PostMapping("registration") // 작업실적 등록
     public Map<String, Object> WorkPerformanceRegistration(@RequestBody String json) throws JSONException {
@@ -28,10 +54,9 @@ public class WorkPerformanceController {
         JSONObject obj = new JSONObject(json);
         Long workOrderId = Long.parseLong(obj.getString("workOrderId"));
         Integer currentWorkload = Integer.parseInt(obj.getString("currentWorkload"));
-        String usedProductCode = obj.getString("usedProductCode");
-        Integer usedProductAmount = Integer.parseInt(obj.getString("usedProductAmount"));
+        JSONArray usedProductArray = obj.getJSONArray("usedProduct");
 
-        switch (workPerformanceService.uploadWorkPerformance(workOrderId, currentWorkload, usedProductCode, usedProductAmount)) {
+        switch (workPerformanceService.uploadWorkPerformance(workOrderId, currentWorkload, usedProductArray)) {
             case -1:
                 response.put("success", false);
                 response.put("message", "빈칸을 모두 채워주세요.");
@@ -43,6 +68,18 @@ public class WorkPerformanceController {
             case -3:
                 response.put("success", false);
                 response.put("message", "부품코드가 존재하지 않습니다.");
+                return response;
+            case -4:
+                response.put("success", false);
+                response.put("message", "작업량이 0을 초과해야 합니다.");
+                return response;
+            case -5:
+                response.put("success", false);
+                response.put("message", "재고가 부족합니다.");
+                return response;
+            case -6:
+                response.put("success", false);
+                response.put("message", "이미 완료된 작업지시입니다.");
                 return response;
             case 0:
                 response.put("success", true);
--- src/main/java/com/simmongs/workperformance/WorkPerformanceService.java
+++ src/main/java/com/simmongs/workperformance/WorkPerformanceService.java
@@ -5,10 +5,14 @@ import com.simmongs.product.Products;
 import com.simmongs.workorder.WorkOrderRepository;
 import com.simmongs.workorder.WorkOrders;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.json.JSONParser;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,23 +33,60 @@ public class WorkPerformanceService {
     }
 
     @Transactional
-    public int uploadWorkPerformance(Long workOrderId, Integer currentWorkload, String usedProductCode, Integer usedProductAmount) {
+    public int uploadWorkPerformance(Long workOrderId, Integer currentWorkload, JSONArray usedProduct) {
 
-        if( workOrderId == null || currentWorkload == null || usedProductCode.trim().equals("") || usedProductAmount == null )
-            return -1;
+        for(int i=0; i<usedProduct.length(); i++) {
 
-        Optional<WorkOrders> workOrders = workOrderRepository.findByWorkOrderId(workOrderId);
-        if (!workOrders.isPresent()) // WorkOrder 없을 경우
-            return -2;
+            JSONObject obj = usedProduct.getJSONObject(i);
 
-        Optional<Products> products = productRepository.findByProductCode(usedProductCode);
-        if (!products.isPresent()) // UsedProductCode 없을 경우
-            return -3;
+            String usedProductCode = obj.getString("usedProductCode");
+            Integer usedProductAmount = Integer.parseInt(obj.getString("usedProductAmount"));
 
-        LocalDateTime workPerformanceDate = LocalDateTime.now();
+            if (workOrderId == null || currentWorkload == null || usedProductCode.trim().equals("") || usedProductAmount == null)
+                return -1; // null 체크
 
-        WorkPerformance workPerformance = new WorkPerformance(workOrderId, currentWorkload, usedProductCode, usedProductAmount, workPerformanceDate);
-        workPerformanceRepository.save(workPerformance);
+            Optional<Products> productsCheck = productRepository.findByProductCode(usedProductCode);
+            if (!productsCheck.isPresent())
+                return -3; // UsedProductCode 없을 경우
+
+            Products products = productRepository.getByProductCode(usedProductCode);
+            if (products.getProductAmount() <= 0)
+                return -5; // 재고가 부족할 경우
+        }
+
+        Optional<WorkOrders> workOrdersCheck = workOrderRepository.findByWorkOrderId(workOrderId);
+        if (!workOrdersCheck.isPresent())
+            return -2; // WorkOrder 없을 경우
+
+        if (currentWorkload <= 0)
+            return -4; // 작업량 0 이하일 경우
+
+        WorkOrders workOrders = workOrderRepository.getByWorkOrderId(workOrderId);
+        if (workOrders.getWorkTargetQuantity() <= workOrders.getWorkCurrentQuantity())
+            return -6; // 목표 수량이 이미 달성된 경우
+
+        /*if (workOrders.getWorkCurrentQuantity() + currentWorkload >= workOrders.getWorkTargetQuantity())
+            return -7;*/
+
+        for(int i=0; i<usedProduct.length(); i++) {
+
+            JSONObject obj = usedProduct.getJSONObject(i);
+
+            String usedProductCode = obj.getString("usedProductCode");
+            Integer usedProductAmount = Integer.parseInt(obj.getString("usedProductAmount"));
+
+            // 작업 실적 등록
+            LocalDateTime workPerformanceDate = LocalDateTime.now();
+            WorkPerformance workPerformance = new WorkPerformance(workOrderId, currentWorkload, usedProductCode, usedProductAmount, workPerformanceDate);
+            workPerformanceRepository.save(workPerformance);
+
+            // 재고 개수 차감
+            Products products = productRepository.getByProductCode(usedProductCode);
+            products.amountUpdate(usedProductAmount);
+        }
+
+        // 작업 지시의 현재 작업량 갱신
+        workOrders.workCurrentQuantityUpdate(currentWorkload);
 
         return 0;
     }
