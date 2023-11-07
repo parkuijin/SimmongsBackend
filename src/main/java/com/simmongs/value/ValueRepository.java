30f1bdc0fb64f19d6bb81847be59f99a1cc2a6a4
uijinpark <uj.park@icloud.com>
2023-11-07 11:51:02 +0900

    chore : Value 테이블 삭제

--- src/main/java/com/simmongs/value/ValueRepository.java
+++
@@ -1,8 +0,0 @@
-package com.simmongs.value;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ValueRepository extends JpaRepository<Value, Long> {
-}
